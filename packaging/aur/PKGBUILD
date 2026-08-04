3b3f60bf8969510e7e418b48b1e40aa1428547dd
William Theesfeld <william@theesfeld.net>
2026-08-04 07:43:10 -0400

    chore: packaging homebrew/aur pins for v0.1.13 release assets

--- packaging/aur/PKGBUILD
+++ packaging/aur/PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: William Theesfeld <william@theesfeld.net>
 pkgname=joule-bin
-pkgver=0.1.12
+pkgver=0.1.13
 pkgrel=1
 pkgdesc="Donate idle compute, earn millijoules, use open-weight AI (prebuilt binary)"
 arch=('x86_64' 'aarch64')
@@ -11,8 +11,8 @@ conflicts=('joule')
 options=('!strip')
 source_x86_64=("https://github.com/f00-sh/joule/releases/download/v${pkgver}/joule-${pkgver}-linux-x86_64.tar.gz")
 source_aarch64=("https://github.com/f00-sh/joule/releases/download/v${pkgver}/joule-${pkgver}-linux-aarch64.tar.gz")
-sha256sums_x86_64=('91c7862f473a618c7e05181fded7eb2a680446248f058727f5e5e46f444d9c46')
-sha256sums_aarch64=('67a1d7a5413a436c9cf0cb9da62b47a9677c3e3ea0de3b84b292db8692ef46ac')
+sha256sums_x86_64=('708c2aa33849bced0180482a376ab26f30662b3220a5bbbdab5da23eff2e6393')
+sha256sums_aarch64=('25b1a3290c4ab444ea5441fcc2adc58f0a3ea1a12a7e5012fe4706bafa4de14e')
 package() {
   cd "${srcdir}"
   root="$(find . -maxdepth 2 -type f -name joule | head -1 | xargs dirname)"
--- packaging/homebrew/joule.rb
+++ packaging/homebrew/joule.rb
@@ -1,26 +1,26 @@
 class Joule < Formula
   desc "Donate idle compute, earn millijoules, use open-weight AI"
   homepage "https://joule.f00.sh/"
-  version "0.1.12"
+  version "0.1.13"
   license "MIT"
   on_macos do
     on_arm do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-darwin-aarch64.tar.gz"
-      sha256 "a3ac7e4b8e4516fc9ff9ef647834bf1e766323c38d8edb114472994af56b4ff3"
+      sha256 "14cdd04259833ec585122592cd54345114384224bdf7390c706cb7582a89baff"
     end
     on_intel do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-darwin-x86_64.tar.gz"
-      sha256 "c60830d9f873940a48dd21697169c1861344c56dd22a49f5829b98ae26ac5bb0"
+      sha256 "99708927092ad341405017b04084a21e5f777ae3bb9539c88a98c5b32f62c390"
     end
   end
   on_linux do
     on_arm do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-linux-aarch64.tar.gz"
-      sha256 "67a1d7a5413a436c9cf0cb9da62b47a9677c3e3ea0de3b84b292db8692ef46ac"
+      sha256 "25b1a3290c4ab444ea5441fcc2adc58f0a3ea1a12a7e5012fe4706bafa4de14e"
     end
     on_intel do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-linux-x86_64.tar.gz"
-      sha256 "91c7862f473a618c7e05181fded7eb2a680446248f058727f5e5e46f444d9c46"
+      sha256 "708c2aa33849bced0180482a376ab26f30662b3220a5bbbdab5da23eff2e6393"
     end
   end
   def install
